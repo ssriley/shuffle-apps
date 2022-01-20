51514a23684efddecbc051ad03cfb81c29a81bdf
ssriley <reid.riley@safesystems.com>
2022-01-20 10:33:26 -0500

    Update app.py

--- active-directory-reid/1.0.0/src/app.py
+++ active-directory-reid/1.0.0/src/app.py
@@ -29,8 +29,8 @@ class ActiveDirectory(AppBase):
     def __ldap_connection(self, server, port, domain, login_user, password, use_ssl):
         use_SSL = False if use_ssl.lower() == "false" else True
         login_dn = domain + "\\" + login_user
-        tls_config = Tls(validate=ssl.CERT_NONE,version=ssl.PROTOCOL_TLSv1,ciphers=Any)
-        s = Server(server, port=int(port), use_ssl=use_SSL, tls=tls_config)
+        tls_config = Tls(validate=ssl.CERT_NONE,version=ssl.PROTOCOL_TLSv1)
+        s = Server(server, port=int(port), use_ssl=use_SSL, tls=None)
         c = Connection(s, user=login_dn, password=password, auto_bind=True)
 
         return c
