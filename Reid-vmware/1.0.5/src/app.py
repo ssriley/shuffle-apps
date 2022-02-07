4496fd95872c0faefb76aa567ff3e227f7c2ac63
ssriley <reid.riley@safesystems.com>
2022-02-07 14:29:14 -0500

    Update app.py

--- Reid-vmware/1.0.5/src/app.py
+++ Reid-vmware/1.0.5/src/app.py
@@ -336,9 +336,9 @@ class VMwareTools(AppBase):
             return json.dumps(result)
     def list_snapshots(self,host_ip,username,password,port,disableSslCertValidation=True,vm_name=None):
         si = self.__connect(host_ip=host_ip,username=username,password=password,port=port,disableSslCertValidation=disableSslCertValidation)
-        # vm = None
-        # # if vm_name:
-        # #     content = si.RetrieveContent()
+        vm = None
+        if vm_name:
+             content = si.RetrieveContent()
         # #     vm = self.get_obj(content, vim.VirtualMachine, vm_name)
 
         # if vm is None:
