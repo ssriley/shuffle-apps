0d8dcafae765503f70fe96f183472f4bf3b55b3d
ssriley <reid.riley@safesystems.com>
2022-02-08 14:03:03 -0500

    Update app.py

--- Reid-vmware/1.0.8/src/app.py
+++ Reid-vmware/1.0.8/src/app.py
@@ -427,7 +427,7 @@ class VMwareTools(AppBase):
             return json.dumps({"Status": "Created snapshot for {0}".format(vm.name),
             "Task": "Result of task ".format(task.info.result)})
         except Exception as err:
-            return json.dumps({"Error was {0}".format(err)})
+            raise Exception(json.dumps({"Error was {0}".format(err)}))
             
 if __name__ == "__main__":
     VMwareTools.run()
