186e8e0ddc74776b3608d251ff9e6ec2bb1c9f84
ssriley <reid.riley@safesystems.com>
2022-02-17 11:07:07 -0500

    Update app.py

--- Reid-vmware/1.1.8/src/app.py
+++ Reid-vmware/1.1.8/src/app.py
@@ -1031,7 +1031,7 @@ class VMwareTools(AppBase):
             #else:
             #nic_spec.device.backing = vim.vm.device.VirtualEthernetCard.NetworkBackingInfo()
             nic_spec.device.backing = virtual_nic_device.backing
-            nic_spec.device.backing.useAutoDetect = False
+            #nic_spec.device.backing.useAutoDetect = False
             #nic_spec.device.backing.network = network
             nic_spec.device.backing.deviceName = network.name
             #nic_spec.device.key = 4000
