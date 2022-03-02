9cb46695147a9fcd33d30feca596002b60e0c0e9
ssriley <reid.riley@safesystems.com>
2022-03-02 14:02:17 -0500

    added proofpoint smartsearch

--- Reid-http/1.2.0/src/app.py
+++ Reid-http/1.2.0/src/app.py
@@ -1,6 +1,5 @@
 import time
 import json
-import json
 import random
 import socket
 import uncurl
---
+++ proofpoint-Reid/1.0.0/src/app.py
@@ -0,0 +1,51 @@
+import json
+import requests
+#from telnetlib import TLS
+import ssl
+
+from walkoff_app_sdk.app_base import AppBase
+
+class ProofPoint(AppBase):
+    __version__ = "1.0.0"
+    app_name = "ProofPoint SmartSearch Reid"  # this needs to match "name" in api.yaml
+
+    def __init__(self, redis, logger, console_logger=None):
+        """
+        Each app should have this __init__ to set up Redis and logging.
+        :param redis:
+        :param logger:
+        :param console_logger:
+        """
+        super().__init__(redis, logger, console_logger)
+
+    def search_email(
+        self,
+        username,
+        password,
+        server,
+        port="10000",
+        action=None,
+        sending_host=None,
+        email_subject=None,
+        sending_ip=None,
+        attachment_name=None,
+        from_address=None,
+        to_address=None,
+        count=100
+    ):
+        url_base = "https://" + server + ":" + port + "/rest/v1/pss/filter"
+        querystring = {
+            'action': action,
+            'host': sending_host,
+            'subject': email_subject,
+            'ip': sending_ip,
+            'attach': attachment_name,
+            'env_from': from_address,
+            'env_rcpt': to_address,
+            'count': count
+        }
+        headers = {"Accept": "application/json"}
+        return requests.get(url_base,headers=headers,auth=(username,password),params=querystring,verify=False).json()
+
+if __name__ == "__main__":
+    ProofPoint.run()
